0212bbaf550d129185af03e9b1ca0164fb9488f0
usama <usamaahmad.cui@gmail.com>
2021-04-21 13:54:24 +0500

    test1

--- BookStore/BookStore/Startup.cs
+++ BookStore/BookStore/Startup.cs
@@ -32,7 +32,7 @@ namespace BookStore
             {
                 endpoints.MapGet("/", async context =>
                 {
-                    await context.Response.WriteAsync("Hello World!");
+                    await context.Response.WriteAsync("Hello Usama!");
                 });
             });
         }
